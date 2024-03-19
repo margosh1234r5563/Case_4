5a4b6d714287219020bb87de0ec0df330a3cd834
akkos <a.kosheleva@g.nsu.ru>
2024-03-19 17:42:15 +0700

    add file

--- witcher.py
+++ witcher.py
@@ -58,20 +58,18 @@ else:
             print(f'{ru.MINIMAL_MONEY} {counter}')
 
 print(ru.ITEMS_1)
-number_items_list = []
+number_items_1 = [1, 2, 3, 4]
 amount_orders_items_list = []
 
 for n in range(1, 5):
-    number_items, amount_orders_items = input().split()
-    number_items_list.append(number_items)
-    amount_orders_items_list.append(amount_orders_items)
+    amount_orders_items_1 = input().split()
+    amount_orders_items_list.append(amount_orders_items_1)
 
 print(ru.BLADE_CATEGORY)
 amount_orders_blade_list = []
-number_blade_list = []
+number_blade_list = [1, 2, 3, 4]
 for k in range(1, 5):
-    number_blade, amount_orders_blade = map(int, input().split())
-    number_blade_list.append(number_blade)
+    amount_orders_blade = int(input())
     amount_orders_blade_list.append(amount_orders_blade)
 
 if sum(amount_orders_blade_list) > 1:
@@ -93,10 +91,9 @@ for number_blade, amount_orders_blade in zip(number_blade_list, amount_orders_bl
 
 print(ru.DRINK_CATEGORY)
 amount_orders_drink_list = []
-number_drink_list = []
+number_drink_list = [1, 2, 3, 4]
 for k in range(1, 5):
-    number_drink, amount_orders_drink = map(int, input().split())
-    number_drink_list.append(number_drink)
+    amount_orders_drink = int(input())
     amount_orders_drink_list.append(amount_orders_drink)
 cost_drink = 0
 
@@ -112,9 +109,9 @@ for number_drink, amount_orders_drink in zip(number_drink_list, amount_orders_dr
             cost_drink += 400 * amount_orders_drink
 
 print(ru.ITEMS_2)
+number_items_2 = [7, 8, 9]
 for n in range(7, 10):
-    number_items, amount_orders = input().split()
-    number_items_list.append(number_items)
+    amount_orders = input().split()
     amount_orders_items_list.append(amount_orders)
 
 cost_items = [22, 100, 5, 1, 10, 30, 10]
@@ -123,8 +120,7 @@ amnt_mny_itms = 0
 phase = 0
 cost_items.insert(5, 1)
 cost_items.insert(6, 1)
-number_items_list.insert(5, cost_blade)
-number_items_list.insert(6, cost_drink)
+number_items_list = [1, 2, 3, 4, cost_blade, cost_drink, 7, 8, 9]
 for s in range(len(cost_items)):
     while amount_money >= amnt_mny_itms:
         amnt_mny_itms += int(cost_items[s]) * int(number_items_list[s])
